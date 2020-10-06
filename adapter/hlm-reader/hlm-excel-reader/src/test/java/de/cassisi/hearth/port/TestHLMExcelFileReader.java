ba7a678ed2b413f35aea42ff2e2f59684af5b797
Domenic Cassisi <domenic.cassisi@web.de>
2020-10-06 20:56:28 +0200

    #8 Use Case: Read HLM Data Fix for FileNotFoundException

--- adapter/hlm-reader/hlm-excel-reader/src/test/java/de/cassisi/hearth/port/TestHLMExcelFileReader.java
+++ adapter/hlm-reader/hlm-excel-reader/src/test/java/de/cassisi/hearth/port/TestHLMExcelFileReader.java
@@ -3,21 +3,22 @@ package de.cassisi.hearth.port;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.util.Objects;
 
 class TestHLMExcelFileReader {
 
     @Test
-    void testSameDay() {
+    void testSameDay() throws URISyntaxException {
         HLMExcelFileReader reader = new HLMExcelFileReader();
-        File excelFile = new File(Objects.requireNonNull(TestHLMExcelFileReader.class.getClassLoader().getResource("hlm_test_data_1.xlsx")).getFile());
+        File excelFile = new File(Objects.requireNonNull(TestHLMExcelFileReader.class.getClassLoader().getResource("hlm_test_data_1.xlsx")).toURI());
         reader.readHLMFile(excelFile);
     }
 
     @Test
-    void testMidnight() {
+    void testMidnight() throws URISyntaxException {
         HLMExcelFileReader reader = new HLMExcelFileReader();
-        File excelFile = new File(Objects.requireNonNull(TestHLMExcelFileReader.class.getClassLoader().getResource("hlm_test_data_2.xlsx")).getFile());
+        File excelFile = new File(Objects.requireNonNull(TestHLMExcelFileReader.class.getClassLoader().getResource("hlm_test_data_2.xlsx")).toURI());
         reader.readHLMFile(excelFile);
     }
 
