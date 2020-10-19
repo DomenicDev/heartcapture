8d7b7ed020660fee08e46b228af1bdfd4ba6c88a
Domenic Cassisi <domenic.cassisi@web.de>
2020-10-19 10:14:09 +0200

    #6 Reports can now be saved on disk

--- application/fxapp/src/main/java/de/cassisi/hearth/ui/presenter/GenerateReportPresenter.java
+++ application/fxapp/src/main/java/de/cassisi/hearth/ui/presenter/GenerateReportPresenter.java
@@ -1,10 +1,11 @@
 package de.cassisi.hearth.ui.presenter;
 
 import de.cassisi.hearth.usecase.output.OutputHandler;
+import javafx.stage.FileChooser;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 
 import static de.cassisi.hearth.usecase.GenerateReport.OutputData;
@@ -16,7 +17,11 @@ public class GenerateReportPresenter implements OutputHandler<OutputData> {
     @Override
     public void handle(OutputData outputData) {
         try {
-            FileOutputStream fos = new FileOutputStream("build/testexport.xlsx");
+
+            FileChooser fileChooser = new FileChooser();
+            File fileToSave = fileChooser.showSaveDialog(null);
+
+            FileOutputStream fos = new FileOutputStream(fileToSave);
             fos.write(outputData.reportFile);
             fos.flush();
             fos.close();
