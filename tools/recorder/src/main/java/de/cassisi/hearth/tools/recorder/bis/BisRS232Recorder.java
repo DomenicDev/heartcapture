43555a1b6ec790195105335bf505551dd1794696
Domenic Cassisi <domenic.cassisi@web.de>
2020-11-20 16:49:30 +0100

    #8 Use Case: Read HLM Data Some cleanup of methods

--- tools/recorder/src/main/java/de/cassisi/hearth/tools/recorder/bis/BisRS232Recorder.java
+++ tools/recorder/src/main/java/de/cassisi/hearth/tools/recorder/bis/BisRS232Recorder.java
@@ -9,22 +9,28 @@ import java.util.regex.Pattern;
 
 public class BisRS232Recorder extends AbstractRS232Recorder<BISData> {
 
-    private final String regex = "^(?:.*?\\|){13}\\d*\\s(\\d*\\.\\d).*$";
+    private final String regex = "\\d{2}\\/\\d{2}\\/\\d{4}\\s\\d{2}:\\d{2}:\\d{2}\\|\\s*\\d+\\|\\s*\\d+\\|\\w+\\s*\\|\\w+\\s+\\|\\s+\\d+\\|\\s+\\d+\\|\\w+\\s*\\|\\s+\\d+.\\d+\\|\\s+\\d+.\\d+\\|\\s+\\S{4}\\|\\s+(\\d+.\\d)";
     private final Pattern pattern = Pattern.compile(regex);
 
     public BisRS232Recorder(String port) {
-        super(port, new byte[] {'\n'}, true);
+        super(port, new byte[] {0x0D, 0x0A}, true);
     }
 
     @Override
     protected BISData convert(String data) throws ConversionException {
-        Matcher matcher = pattern.matcher(data.trim());
-        if (matcher.find()) {
-            String depthOfAnesthesia = matcher.group(1);
-            double value = Double.parseDouble(depthOfAnesthesia);
-            return new BISData(LocalDateTime.now(), value);
-        } else {
-            throw new ConversionException(data, BISData.class);
+        String input = data.trim();
+        Matcher matcher = pattern.matcher(input);
+        try {
+            if (matcher.find()) {
+                String depthOfAnesthesia = matcher.group(1);
+                double value = Double.parseDouble(depthOfAnesthesia);
+                return new BISData(LocalDateTime.now(), value);
+            } else {
+                throw new ConversionException(data, BISData.class);
+            }
+
+        } catch (Exception e) {
+           return null;
         }
 
     }
