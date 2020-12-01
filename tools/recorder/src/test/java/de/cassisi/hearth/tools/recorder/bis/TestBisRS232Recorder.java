a1b6b59d1a2e9719c85949bdb747578ca62f1a3a
Domenic Cassisi <domenic.cassisi@web.de>
2020-12-01 19:48:45 +0100

    Fixed test case

--- tools/recorder/src/test/java/de/cassisi/hearth/tools/recorder/bis/TestBisRS232Recorder.java
+++ tools/recorder/src/test/java/de/cassisi/hearth/tools/recorder/bis/TestBisRS232Recorder.java
@@ -9,12 +9,12 @@ public class TestBisRS232Recorder {
     @Test
     void testConversion() {
         BisRS232Recorder recorder = new BisRS232Recorder("NULL");
-        String testData = "09/18/2020 14:01:52|\t10|\t27|On\t|None\t|Off\t|Off\t|No\t|\t0.8|\t14.8|\t0010|\t52.7|\t54.7|\t38.8|\t94.9|\t3|000000080|   0.0|\t0.0|\t8000|\t0.0|\t0.0|\t0.0|\t100.0|\t5|00000080|\t0.8|\t14.8|\t0010|\t52.7|\t54.7|\t38.81|\t94.91\t0|00000080|";
+        String testData = " 11/19/2020 14:22:07|      10|      27|On      |Low     |      40|      60|Yes     |     5.9|    14.9|    1010|    28.8|    63.4|    26.4|    90.4|      10|00000080|     0.0|     0.0|    8000|     0.0|     0.0|     0.0|   100.0|       5|00000080|     5.9|    14.9|    1010|    28.8|    63.4|    26.4|    90.4|       0|00000080|";
 
         BISData result = recorder.convert(testData);
         double bsi = result.depthOfAnesthesia;
 
-        assertEquals(38.8, bsi);
+        assertEquals(28.8, bsi);
     }
 
 }
